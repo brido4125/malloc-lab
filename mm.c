90c2ff392285dc811b5294d1e9c160a05d9d9aac
brido4125 <hcs4125@gmail.com>
2022-10-28 15:19:14 +0900

    update:free와 콜레싱 함수 구현

--- mm.c
+++ mm.c
@@ -128,6 +128,41 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    size_t size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size,0));
+    PUT(FTRP(bp), PACK(size,0)));
+    coalesce(bp);
+}
+
+static void *coalesce(void *bp){
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp))));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp))));
+
+    size_t size = GET_SIZE(HDRP(bp));//현재 블록의 사이즈
+
+    //양쪽 모두 할당된 경우 -> coalescing할 공간이 없다
+    if (prev_alloc && next_alloc) {
+        return bp;
+
+    }
+    // next가 Free인 경우
+    else if (prev_alloc && !next_alloc) {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size,0));
+        PUT(FTRP(bp), PACK(size,0));
+    } else if (!prev_alloc && next_alloc) {
+        size += GET_SIZE(FTRP(PREV_BLKP(bp))));
+        PUT(FTRP(bp), PACK(size, 0)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size,0));
+        bp = PREV_BLKP(bp);
+    }else{
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp))));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size,0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size,0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
 }
 
 /*
