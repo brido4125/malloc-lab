7e4bfd03ebc570bfd6f1b51b980c3ec846e4ce05
brido4125 <hcs4125@gmail.com>
2022-11-02 22:00:57 +0900

    fix:removeBlock30

--- mm.c
+++ mm.c
@@ -326,11 +326,9 @@ void removeBlock(void* bp){
  * 만일 이미 할당된 메모리 영역에서 크기를 조정할 수 있다면(옆 블럭이 가용블럭 or epilogue) 반환되는 주소는 첫 번째 인자로 전달된 주소와 같다.
  * 그러나 불가능하다면 기존의 메모리를 해제하고 새로운 영역에 다시 할당한 후, 새로 할당된 메모리의 주소를 반환한다.
  */
-
-
 void *mm_realloc(void *ptr, size_t size)
 {
-    printf("here is realloc");
+    printf("here is realloc\n");
     if (size < 0) {
         return NULL;
     }
@@ -348,19 +346,19 @@ void *mm_realloc(void *ptr, size_t size)
     if (new_size <= old_size) {
         return ptr;
     }
-    int remain = old_size - new_size;
+//    int remain = old_size - new_size;
     /*
     * remain이 16 이상인 경우,남은 공간을 때어서 가용 공간으로 바꿔줘야한다.
     * */
-    if (remain > 2 * DSIZE) {
-        PUT(HDRP(ptr),PACK(new_size,1));
-        PUT(FTRP(ptr),PACK(new_size,1));
-        PUT(HDRP(NEXT_BLKP(ptr)), PACK(remain,0));
-        PUT(FTRP(NEXT_BLKP(ptr)), PACK(remain,0));
-        void *new_remain_block = NEXT_BLKP(ptr);
-        putFreeBlock(new_remain_block);
-        return new_remain_block;
-    }
+//    if (remain > 2 * DSIZE) {
+//        PUT(HDRP(ptr),PACK(new_size,1));
+//        PUT(FTRP(ptr),PACK(new_size,1));
+//        PUT(HDRP(NEXT_BLKP(ptr)), PACK(remain,0));
+//        PUT(FTRP(NEXT_BLKP(ptr)), PACK(remain,0));
+//        void *new_remain_block = NEXT_BLKP(ptr);
+//        putFreeBlock(new_remain_block);
+//        return new_remain_block;
+//    }
         /*
          * remain이 음수인 경우, 즉 현재 블럭의 공간으로 realloc이 요구하는 사이즈를 감당하지 못하는 경우
          * 새로운 malloc을 통해 주소를 새롭게 할당 받아야 한다.
